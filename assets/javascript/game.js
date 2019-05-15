5c23573fb11e023a113535aafa329a39399f7093
armjim14 <ajimenez1497@gmail.com>
2019-05-15 11:25:48 -0700

    finction youlost acting up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 var time = 12;
-var li = 0;
 var start = 0
 var keeptrack = 0;
 
@@ -30,17 +29,12 @@ document.onkeyup = function(e) {
 }
 
 function press() {  
-    li +=1;
     document.getElementById("number").innerHTML = time;
     
     if ( time >= 1 ){
         time -= 1;
     } else {
-        time = 12;
-        gues.textContent = " ";
-        dots.textContent = "";
-        start -= 1;
-        lose += 1;
+        youlost();
     }
     loser.textContent = lose;
     win.textContent = winner;
@@ -53,8 +47,9 @@ function nextLevel(norm) {
         || (norm == "U") || (norm == "V") || (norm == "W") || (norm == "X") || (norm == "Y") || (norm == "Z") ) {
 
         var userchoice = " " + norm;
-
-        gues.textContent = userchoice;
+        var newspan = document.createElement("span");
+        newspan.textContent = userchoice;
+        gues.appendChild(newspan);
 
         press(); 
     } 
@@ -78,44 +73,57 @@ function checkletter(lett) {
     console.log(keeptrack);
 
     if ( word[0] == lett ) {
-        other.style.display = "block";
         other.textContent = "first spot";
         keeptrack += 1;
     } else if ( word[1] == lett ) {
-        other.style.display = "block";
         other.textContent = ("second spot");
         keeptrack += 1;
     } else if ( word[2] == lett ) {
-        other.style.display = "block";
         other.textContent = ("third spot");
         keeptrack += 1;
     } else if ( word[3] == lett ) {
-        other.style.display = "block";
         other.textContent = ("fourth spot");
         keeptrack += 1;
     } else if ( word[4] == lett ) {
-        other.style.display = "block";
         other.textContent = ("fifth spot");
         keeptrack += 1;
     } else if ( word[5] == lett ) {
-        other.style.display = "block";
         other.textContent = ("sixth spot");
         keeptrack += 1;
     } else if ( word[6] == lett ) {
-        other.style.display = "block";
         other.textContent = ("seventh spot");
         keeptrack += 1;
     } else if ( word[7] == lett ) {
-        other.style.display = "block";
         other.textContent = ("eight spot");
         keeptrack += 1;
     } else {
-        other.style.display = "block";
         other.textContent = ("No match");
     }
 
-    // if (keeptrack = word.length) {
-    //     winner += 1;
-    // }
+    if (keeptrack == word.length) {
+        youwon();
+    }
 
+}
+
+function youwon() {
+    keeptrack = 0; 
+    start -= 1;
+    time = 12;
+    winner += 1;
+    gues.textContent = "";
+    dots.textContent = "";
+    other.textContent = "You won!, The word was " + word + " press space to play again";
+    win.textContent = winner;
+}
+
+function youlost() {
+    keeptrack = 0;
+    start -= 1;
+    time = 12;
+    lose += 1;
+    gues.textContent = "";
+    dots.textContent = "";
+    other.textContent = "Sorry you lost, The word was " + word + " press space to play again";
+    loser.textContent = lose;
 }
