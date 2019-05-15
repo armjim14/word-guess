fa4b46feec56bded779a65950f482c328aa885f1
armjim14 <ajimenez1497@gmail.com>
2019-05-15 10:30:13 -0700

    var win needs work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
 var time = 12;
 var li = 0;
 var start = 0
+var keeptrack = 0;
+
+var choice = ["KitKat", "Twix", "Hershey", "Snickers", "Crunch", "Pay", "Reeses", "mnms", "kinder", "cocoa"];
+var what = Math.floor(Math.random() * 10);
+var word = (choice[what].toUpperCase());
 
 var other = document.getElementById("left-text");
 var gues = document.getElementById("guess");
@@ -10,23 +15,17 @@ var winner = 0;
 var loser = document.getElementById("loser");
 var lose = 0;
 
-win.textContent = winner;
-
 document.onkeyup = function(e) {
     var letter = e.key.toUpperCase();
 
-    var choice = ["KitKat", "Twix", "Hershey", "Snickers", "Crunch", "Pay", "Reeses", "mnms", "kinder", "cocoa"];
-    var what = Math.floor(Math.random() * 10);
-    var word = (choice[what].toUpperCase());
-
     if ( start == 0 ){
         start =+ 1;
-        getword(word);
+        getword();
         nextLevel(letter);
-        checkletter(letter, word);
+        checkletter(letter);
     } else {
         nextLevel(letter);
-        checkletter(letter, word);
+        checkletter(letter);
     }
 }
 
@@ -44,6 +43,7 @@ function press() {
         lose += 1;
     }
     loser.textContent = lose;
+    win.textContent = winner;
 }
 
 function nextLevel(norm) {
@@ -53,38 +53,69 @@ function nextLevel(norm) {
         || (norm == "U") || (norm == "V") || (norm == "W") || (norm == "X") || (norm == "Y") || (norm == "Z") ) {
 
         var userchoice = " " + norm;
-        var span2 = document.createElement("span");
 
-        span2.textContent = (userchoice);
-        gues.appendChild(span2);
+        gues.textContent = userchoice;
 
         press(); 
     } 
 }
 
-function getword(upword) {
+function getword() {
+    what = Math.floor(Math.random() * 10);
+    word = (choice[what].toUpperCase());
 
-    for ( var i = 0; i < upword.length; i++ ) {
+    for ( var i = 0; i < word.length; i++ ) {
         var span = document.createElement("Span");
         span.setAttribute("id", i );
-        span.textContent = upword[i];
+        span.textContent = word[i];
         dots.appendChild(span);
     }
 }
 
-function checkletter(lett, wood) {
+function checkletter(lett) {
 
-    console.log(wood);
-    console.log(lett);
+    console.log(word.length);
+    console.log(keeptrack);
 
-    if ( wood[0] == lett ) {
+    if ( word[0] == lett ) {
         other.style.display = "block";
-        other.textContent = "it worked";
-    } else if ( wood[1] == lett ) {
+        other.textContent = "first spot";
+        keeptrack += 1;
+    } else if ( word[1] == lett ) {
         other.style.display = "block";
-        other.textContent = ("it worked paret 2");
+        other.textContent = ("second spot");
+        keeptrack += 1;
+    } else if ( word[2] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("third spot");
+        keeptrack += 1;
+    } else if ( word[3] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("fourth spot");
+        keeptrack += 1;
+    } else if ( word[4] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("fifth spot");
+        keeptrack += 1;
+    } else if ( word[5] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("sixth spot");
+        keeptrack += 1;
+    } else if ( word[6] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("seventh spot");
+        keeptrack += 1;
+    } else if ( word[7] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("eight spot");
+        keeptrack += 1;
     } else {
-        other.textContent = ("it does not work");
+        other.style.display = "block";
+        other.textContent = ("No match");
     }
+
+    // if (keeptrack = word.length) {
+    //     winner += 1;
+    // }
 
 }
