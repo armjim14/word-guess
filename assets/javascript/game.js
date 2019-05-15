45b201084265fe4bfce13bc72e4c869c1911e13b
armjim14 <ajimenez1497@gmail.com>
2019-05-15 14:18:25 -0700

    error in console for style.null thing but else it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var start = 0
 var keeptrack = 0;
 var rtime = 0;
 
-var choice = ["KitKat", "Twix", "Hershey", "Snickers", "Crunch", "Pay", "Reeses", "mnms", "kinder", "cocoa"];
+var choice = ["Mars", "Twix", "Patchi", "Dove", "York", "Cadbury", "Mounds", "Bounty", "kinder", "Chunky"];
 var what = Math.floor(Math.random() * 10);
 var word = (choice[what].toUpperCase());
 
@@ -22,12 +22,12 @@ var lose = 0;
 document.onkeyup = function(e) {
     var letter = e.key.toUpperCase();
 
+    console.log(word);
+
     other.textContent = "";
     loser.textContent = lose;
     win.textContent = winner;
     num.textContent = time;
-
-    console.log(arr);
 
     if ( start == 0 ){
         start =+ 1;
@@ -37,9 +37,10 @@ document.onkeyup = function(e) {
         other.textContent = "that " + letter + " is already selected";
     } else {
         
-        if ( (letter == "A") || (letter == "B") || (letter == "C") || (letter == "D") || (letter == "E") || (letter == "F") || (letter == "G") || (letter == "H") || (letter == "I") || (letter == "J")
-        || (letter == "K") || (letter == "L") || (letter == "M") || (letter == "N") || (letter == "O") || (letter == "P") || (letter == "Q") || (letter == "R") || (letter == "S") || (letter == "T")
-        || (letter == "U") || (letter == "V") || (letter == "W") || (letter == "X") || (letter == "Y") || (letter== "Z") ) {
+        if ( (letter == "A") || (letter == "B") || (letter == "C") || (letter == "D") || (letter == "E") || (letter == "F") || (letter == "G") || (letter == "H") 
+        || (letter == "I") || (letter == "J") || (letter == "K") || (letter == "L") || (letter == "M") || (letter == "N") || (letter == "O") || (letter == "P") 
+        || (letter == "Q") || (letter == "R") || (letter == "S") || (letter == "T") || (letter == "U") || (letter == "V") || (letter == "W") 
+        || (letter == "X") || (letter == "Y") || (letter== "Z") ) {
 
             arr[rtime] = letter;
 
@@ -64,14 +65,12 @@ function getword() {
         var span = document.createElement("Span");
         span.setAttribute("id", i );
         span.textContent = word[i];
+        document.getElementById(Number).style.display = "none";
         dots.appendChild(span);
     }
 }
 
 function checkletter(lett) {
-
-    console.log(word.length);
-    console.log(keeptrack);
 
     if ( word[0] == lett ) {
         other.textContent = "first spot";
