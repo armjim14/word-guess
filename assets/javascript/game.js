5416fd9c0de48bbc22402469bbafd87325fc0cbb
armjim14 <ajimenez1497@gmail.com>
2019-05-14 21:55:55 -0700

    lett and wood connection is working now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 var time = 12;
 var li = 0;
 var start = 0
-var arr = [];
 
 var other = document.getElementById("left-text");
 var gues = document.getElementById("guess");
@@ -16,29 +15,29 @@ win.textContent = winner;
 document.onkeyup = function(e) {
     var letter = e.key.toUpperCase();
 
+    var choice = ["KitKat", "Twix", "Hershey", "Snickers", "Crunch", "Pay", "Reeses", "mnms", "kinder", "cocoa"];
+    var what = Math.floor(Math.random() * 10);
+    var word = (choice[what].toUpperCase());
+
     if ( start == 0 ){
         start =+ 1;
+        getword(word);
         nextLevel(letter);
-        getword();
+        checkletter(letter, word);
     } else {
         nextLevel(letter);
+        checkletter(letter, word);
     }
 }
 
-function simple() {
-    li +=1
-    press();
-}
-
 function press() {  
-
+    li +=1;
     document.getElementById("number").innerHTML = time;
     
     if ( time >= 1 ){
         time -= 1;
     } else {
         time = 12;
-        arr = [];
         gues.textContent = " ";
         dots.textContent = "";
         start -= 1;
@@ -52,30 +51,40 @@ function nextLevel(norm) {
     if ( (norm == "A") || (norm == "B") || (norm == "C") || (norm == "D") || (norm == "E") || (norm == "F") || (norm == "G") || (norm == "H") || (norm == "I") || (norm == "J")
         || (norm == "K") || (norm == "L") || (norm == "M") || (norm == "N") || (norm == "O") || (norm == "P") || (norm == "Q") || (norm == "R") || (norm == "S") || (norm == "T")
         || (norm == "U") || (norm == "V") || (norm == "W") || (norm == "X") || (norm == "Y") || (norm == "Z") ) {
-        
-        other.style.display = "none";
 
         var userchoice = " " + norm;
-        arr[li] = userchoice;
+        var span2 = document.createElement("span");
 
-        gues.textContent = arr;
+        span2.textContent = (userchoice);
+        gues.appendChild(span2);
 
-        simple(); 
-    } else {
-        other.style.display = "block";
-        other.textContent = "Not a valid key";
+        press(); 
+    } 
+}
+
+function getword(upword) {
+
+    for ( var i = 0; i < upword.length; i++ ) {
+        var span = document.createElement("Span");
+        span.setAttribute("id", i );
+        span.textContent = upword[i];
+        dots.appendChild(span);
     }
 }
 
-function getword() {
-    var choice = ["KitKat", "Twix", "Hershey", "Snickers", "Crunch", "Pay", "Reeses", "mnms", "kinder", "cocoa"];
-    var what = Math.floor(Math.random() * 10);
-    var last = (choice[what].toUpperCase());
+function checkletter(lett, wood) {
 
-    for ( var i = 0; i < last.length; i++ ) {
-        var span = document.createElement("Span");
-        span.setAttribute("id", i );
-        span.textContent = last[i];
-        dots.appendChild(span);
+    console.log(wood);
+    console.log(lett);
+
+    if ( wood[0] == lett ) {
+        other.style.display = "block";
+        other.textContent = "it worked";
+    } else if ( wood[1] == lett ) {
+        other.style.display = "block";
+        other.textContent = ("it worked paret 2");
+    } else {
+        other.textContent = ("it does not work");
     }
+
 }
